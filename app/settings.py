0d310fcd29d981cdd70e79a837aa97bf10a199f2
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-06-26 01:34:47 +0800

    Fix error

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-7_&xaz^@5w92#^v8@7-=fz7k)0d0eqgeelxqqpm$$495#3ua&-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 
 
 # Application definition
