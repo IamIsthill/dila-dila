a27b017d77c360b826c6d9fa42b1736c2802e9db
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-10-18 09:57:05 +0800

    Remove delete

--- patients/urls.py
+++ patients/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<int:pk>/', views.patient, name="patient"),
     path('add/', views.add_patient_view, name="add-patient"),
     path('edit/<int:pk>/', views.edit_patient_view, name="edit-patient"),
-    path('delete/<int:pk>/', views.delete_patient_view, name="delete-patient")
+    # path('delete/<int:pk>/', views.delete_patient_view, name="delete-patient")
 ]
